8db116026fe5938f7aba7d6a96380e35461b3cd4
Kishore Vaddadi <vaddadi@karlmax-berlin.com>
2025-06-12 23:24:40 +0200

    final changes

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,8 +1,11 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+  password: varchar({ length: 255 }).notNull(),
+  profile_picture: varchar({ length: 512 }), 
+  created_at: timestamp({ withTimezone: true }).notNull().defaultNow(),
+  updated_at: timestamp({ withTimezone: true }).notNull().defaultNow(),
 });
